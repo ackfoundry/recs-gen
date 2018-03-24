81c8986652fc2b7f8196cf5f70c07925520903a0
Michael Kelly <michaelkelly322@gmail.com>
2018-03-23 22:12:55 -0400

    Add ingress_map and first full solution

--- lib/container.rb
+++ lib/container.rb
@@ -1,42 +1,62 @@
-module Container
+class Container < OpenStruct
   def self.extract_from(task, task_definition, container_definition, network_bindings, options, id_ip_map)
     $logger.debug "Extracting #{container_definition.name}"
 
     if container_definition.name.downcase == options[:task].downcase
-      $logger.debug 'Container is own container, skipping'
+      $logger.warn 'Container is own container, skipping'
       return
     end
 
     net_bindings = network_bindings[container_definition.name]
-    if net_bindings.empty?
-      $logger.debug 'Container has no network bidings, skipping'
+    if net_bindings.nil? || net_bindings.empty?
+      $logger.warn 'Container has no network bidings, skipping'
       return
     end
 
     env = extract_env(container_definition.environment)
-    unless raw_hosts = env[options['host-var']&.upcase]
+    unless raw_hosts = env[options['host-var']&.upcase]&.split(' ')
       $logger.warn "[#{container_definition.name}] #{options['host-var'].upcase} environment variable not found, skipping"
       return
     end
-    unless raw_ports = env['VIRTUAL_PORTS']
+    unless raw_ports = env['VIRTUAL_PORTS']&.split(' ')
       raw_ports = '7519 7520'
       $logger.warn "[#{container_definition.name}] VIRTUAL_PORTS environment variable not found, skipping"
       return
     end
 
-    return OpenStruct.new(
+    host_ports = extract_host_ports(raw_ports, net_bindings)
+
+    return new(
       name: container_definition.name,
-      hosts: raw_hosts.split(' '),
-      ports: raw_ports.split(' '),
+      hosts: raw_hosts,
+      ports: extract_host_ports(raw_ports, net_bindings),
       env: env,
-      address: id_ip_map[task.container_instance_arn]
+      address: id_ip_map[task.container_instance_arn],
+      ingress_map: build_ingress_map(raw_ports, host_ports, raw_hosts)
     )
+  end
+
+  def self.build_ingress_map(raw_ports, host_ports, hostnames)
+    raw_ports.inject({}) do |map, rp|
+      i = raw_ports.index(rp)
+      map[rp.to_i] = {
+        hostname: hostnames[i],
+        port: host_ports[i]
+      }
+      map
+    end
   end
 
   def self.extract_env(kvpairs)
     kvpairs.inject({}) do |env, pair|
       env[pair.name] = pair.value
       env
+    end
+  end
+
+  def self.extract_host_ports(virtual_ports, network_bindings)
+    virtual_ports.map do |vp|
+      network_bindings.find {|b| b.container_port == vp.to_i }&.host_port
     end
   end
 
